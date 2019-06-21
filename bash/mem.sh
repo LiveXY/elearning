4d111409eb829dde5bd0c7cddfda8457d489a502
hcxiong <cexo255@163.com>
2019-06-21 20:42:35 +0800

    no message

--- bash/mem.sh
+++ bash/mem.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -o nounset
+set -o errexit
+
 ps -C $1 -O rss | gawk '{ count ++; sum += $2 }; END {count --; print "Number of processes =",count; print "Memory usage per process =",sum/1024/count, "MB"; print "Total memory usage =", sum/1024, "MB" ;};'
 
 #./mem.sh php-fpm
